490f95f786498f5c2ec9901742d15e650b33583c
crowdbotics-dev <engineering@crowdbotics.com>
2024-04-19 12:13:55 +0000

    Update models

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import (
+    TestModelViewSet,
+    TestModelViewSet,
+    TestModelViewSet,
+    TestModelViewSet,
+)
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +15,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register("testmodel", TestModelViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class TestModel(models.Model):
+    "Generated Model"
+    test = models.TextField()
